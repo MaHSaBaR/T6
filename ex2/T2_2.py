32df0374ecb9c5bef3067a3a97dbc262709ce9d8
MaHSaBaR <157877940+MaHSaBaR@users.noreply.github.com>
2024-01-27 13:31:15 +0330

    Update T2_2.py

--- ex2/T2_2.py
+++ ex2/T2_2.py
@@ -1,110 +1,77 @@
-def sum():
-    l = []
-    a = 0
-    s = 0
-    while a != 'end':
-        l.append(a)
-        a = input()
-    for i in range(len(l)):
-        s += int(l[i])
-    return s 
+def calculate_sum():
+    numbers = []
+    num = 0
+    while num != 'end':
+        numbers.append(int(num))
+        num = input()
+    return sum(numbers)
 
+def calculate_average():
+    numbers = []
+    num = 0
+    while num != 'end':
+        numbers.append(int(num))
+        num = input()
+    return round(sum(numbers) / (len(numbers) - 1), 2)
 
-        
-def average():
-    l = []
-    a = 0
-    s = 0
-    while a != 'end':
-        l.append(a)
-        a = input()
-    for i in range(len(l)):
-        s += int(l[i])
-    return round(s / (len(l) - 1), 2)
-
-def GCD(a ,b):
-    while b != 0: 
-        a , b = b , a % b
+def calculate_gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
     return a
 
+def calculate_gcd_multiple():
+    num = input()
+    result = int(num)
+    while num != 'end':
+        result = calculate_gcd(result, int(num))
+        num = input()
+    return result
 
-def gcd():
-    a = input()
-    w1 = input()
-    w2 = GCD(int(a), int(w1))
-    while w1 != 'end':
-        w2 = GCD(int(w2), int(w1))
-        w1 = input()
-        if w1 == 'end':
-            break
-    return w2
-
-
-def lcd():
-    l = []
-    a = input()
-    while a != 'end':
-        l.append(int(a))
-        a = input()
-        if a == 'end':
-            break
+def calculate_lcd():
+    numbers = []
+    num = input()
+    while num != 'end':
+        numbers.append(int(num))
+        num = input()
     i = 1
-    N = 0
-    while N != len(l):
-        for n in l:
-            if i % n == 0:
-                N += 1
-        if N == len(l):
+    while True:
+        if all(i % n == 0 for n in numbers):
             print(i)
             break
-        else :
-            i += 1
-            N = 0
+        i += 1
 
-def max():
-    l = []
-    a = input()
-    while a != 'end':
-        l.append(int(a))
-        a = input()
-        if a == 'end':
-            break
-    while len(l) > 1:
-        for n in l:
-            for m in l:
-                if n > m:
-                    l.remove(m)
-    return l[0]
+def find_max():
+    numbers = []
+    num = input()
+    while num != 'end':
+        numbers.append(int(num))
+        num = input()
+    return max(numbers)
 
-def min():
-    l = []
-    a = input()
-    while a != 'end':
-        l.append(int(a))
-        a = input()
-        if a == 'end':
-            break
-    while len(l) > 1:
-        for n in l:
-            for m in l:
-                if n < m:
-                    l.remove(m)
-    return l[0]       
-        
+def find_min():
+    numbers = []
+    num = input()
+    while num != 'end':
+        numbers.append(int(num))
+        num = input()
+    return min(numbers)
 
-f = input()
-if f == 'sum':
-    print(sum())
-elif f == 'average':
-    print(average())
-elif f == 'gcd':
-    print(gcd())
-elif f =='lcd':
-    lcd()
-elif f == 'min':
-    print(min())
-elif f == 'max':
-    print(max())
-else:
-    print('Invalid command')
-   
+def main():
+    command = input()
+    if command == 'sum':
+        print(calculate_sum())
+    elif command == 'average':
+        print(calculate_average())
+    elif command == 'gcd':
+        print(calculate_gcd_multiple())
+    elif command == 'lcd':
+        calculate_lcd()
+    elif command == 'min':
+        print(find_min())
+    elif command == 'max':
+        print(find_max())
+    else:
+        print('Invalid command')
+
+if __name__ == "__main__":
+    main()
