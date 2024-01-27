7202fc53db9c81deb1a85183b20e1e70e24991da
MaHSaBaR <157877940+MaHSaBaR@users.noreply.github.com>
2024-01-27 13:34:17 +0330

    Update T3_3.py

--- ex3/T3_3.py
+++ ex3/T3_3.py
@@ -1,30 +1,29 @@
 import re
 
-text = input()
-text = text.rstrip()
-l = list(enumerate(text))
-l1 = []
-l2 = []
-for i in range(len(l)):
-    if l[i][1] == '#':
-        l1.append(l[i][0])
-    if l[i][1] == '@':
-        l2.append(l[i][0])
-for x in l2:
-    for y in range(x):
-        if y in l1:
-            l1.remove(y)
-    if l1 != []:
-        z = min(l1)
-        l1.remove((z))
-        l.remove((z, '#'))
-text = []
-for x in l:
-    text.append(x[1])
-text = ''.join(text)
+def format_text(input_text):
+    input_text = input_text.rstrip()
+    char_enum = list(enumerate(input_text))
+    
+    at_positions = [index for index, char in char_enum if char == '@']
+    hash_positions = [index for index, char in char_enum if char == '#']
 
+    for at_pos in at_positions:
+        for hash_pos in range(at_pos):
+            if hash_pos in hash_positions:
+                hash_positions.remove(hash_pos)
 
-text = re.sub(r'\s{2,}',' ',text)
-text = text.replace(r'\n','\n')
+        if hash_positions:
+            min_hash_pos = min(hash_positions)
+            hash_positions.remove(min_hash_pos)
+            char_enum.remove((min_hash_pos, '#'))
 
-print('Formatted Text: '+ text)
+    formatted_text = ''.join(char for _, char in char_enum)
+    formatted_text = re.sub(r'\s{2,}', ' ', formatted_text)
+    formatted_text = formatted_text.replace(r'\n', '\n')
+
+    return formatted_text
+
+if __name__ == "__main__":
+    input_text = input()
+    formatted_text = format_text(input_text)
+    print('Formatted Text:', formatted_text)
