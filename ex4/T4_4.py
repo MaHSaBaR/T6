d4450663999eddadb00dc29d4c3275d89099364d
MaHSaBaR <157877940+MaHSaBaR@users.noreply.github.com>
2024-01-27 13:40:12 +0330

    Update T4_4.py

--- ex4/T4_4.py
+++ ex4/T4_4.py
@@ -1,9 +1,5 @@
-id_dict = dict()
-time_list = []
-visit_list=[]
-visit_dict = {}
-class patient:
-    def __init__(self,id, name, familyname, age, height, weight,btime):
+class Patient:
+    def __init__(self, id, name, familyname, age, height, weight, btime):
         self.id = id
         self.name = name
         self.familyname = familyname
@@ -12,124 +8,107 @@ class patient:
         self.weight = weight
         self.btime = btime
 
-    def add_patient(self):
-        if str(self.id) in id_dict.keys():
-            print('error: this ID already exists')
+def is_valid_positive_int(value):
+    return value.isdigit() and int(value) > 0
+
+def is_valid_age(value):
+    return is_valid_positive_int(value) or value.lower() == 'unknown'
+
+def is_valid_time(value):
+    return value.isdigit() and 9 <= int(value) <= 18
+
+def is_valid_weight_height(value):
+    return is_valid_positive_int(value) or value.lower() == 'unknown'
+
+def add_patient(patient_id, patient_name, patient_familyname, patient_age, patient_height, patient_weight):
+    if patient_id in patients:
+        print('Error: This ID already exists')
+    elif not is_valid_age(patient_age):
+        print('Error: Invalid age')
+    elif not is_valid_weight_height(patient_height):
+        print('Error: Invalid height')
+    elif not is_valid_weight_height(patient_weight):
+        print('Error: Invalid weight')
+    else:
+        patients[patient_id] = Patient(patient_id, patient_name, patient_familyname, patient_age, patient_height,
+                                      patient_weight, 0)
+        print('Patient added successfully')
+
+def display_patient(patient_id):
+    try:
+        patients[patient_id].display_patient()
+    except KeyError:
+        print('Error: Invalid ID')
+
+def add_visit(patient_id, visit_time):
+    try:
+        patient = patients[patient_id]
+        if not is_valid_time(visit_time):
+            print('Error: Invalid time')
+        elif int(visit_time) in busy_times:
+            print('Error: Busy time')
         else:
-            if int(self.age) < 0 :
-                print('error: invalid age')
-            else:
-                if int(self.height) < 0 :
-                    print('error: invalid height')
-                else:
-                    if int(self.weight) < 0 :
-                        print('error: invalid weight')
-                    else:
-                        id_dict[str(self.id)] = self
-                        print('patient added successfully')
-    
-    def display_patient(self):
-        print('patient name:',self.name)
-        print('patient family name:',self.familyname)
-        print('patient age:',self.age)
-        print('patient height:',self.height)
-        print('patient weight:',self.weight)
-    
-    def add_visit(self):
-        if int(self.btime) in time_list:
-            print('error: busy time')
-        else:
-            if int(self.btime) < 9 or int(self.btime) > 18:
-                print('error: invalid time')
-            else:
-                print('visit added successfully!')
-                time_list.append(int(self.btime))
-                visit_list.append('{}:00 {} {}'.format(self.btime,self.name,self.familyname))
-                visit_dict['{}:00 {} {}'.format(self.btime,self.name,self.familyname)] = self.id
-    
-    def delete_patient(self):
-        for x in visit_list:
-            try:
-                if visit_dict[x] == self.id:
-                    del visit_dict[x]
-            except KeyError:
-                pass
-        if self.btime in time_list:
-            time_list.remove(self.btime)
-        del id_dict[str(self.id)]
-        print('patient deleted successfully!')
-    
+            print('Visit added successfully!')
+            busy_times.append(int(visit_time))
+            visit_list.append('{}:00 {} {}'.format(visit_time, patient.name, patient.familyname))
+            visit_dict['{}:00 {} {}'.format(visit_time, patient.name, patient.familyname)] = patient.id
+    except KeyError:
+        print('Error: Invalid ID')
 
-            
+def delete_patient(patient_id):
+    try:
+        patient = patients[patient_id]
+        for visit in visit_list:
+            if visit_dict[visit] == patient_id:
+                del visit_dict[visit]
+        if patient.btime in busy_times:
+            busy_times.remove(patient.btime)
+        del patients[patient_id]
+        print('Patient deleted successfully!')
+    except KeyError:
+        print('Error: Invalid ID')
 
 
-data = list(filter(None,input().split(' ')))
-while len(data) == 0:
-    print('invalid command')
-    data = list(filter(None,input().split(' ')))
+def display_visits():
+    print('SCHEDULE:')
+    for visit in list(visit_dict.keys()):
+        print(visit)
+
+
+def is_valid_command(command):
+    return len(command) > 0
+
+patients = {}
+busy_times = []
+visit_list = []
+visit_dict = {}
+
+data = list(filter(None, input().split(' ')))
+while not is_valid_command(data):
+    print('Invalid command')
+    data = list(filter(None, input().split(' ')))
 
 while data != ['exit']:
-    if data[0]== 'add' and data[1]== 'patient':
-        p = patient(id=data[2],name=data[3],familyname=data[4],age=data[5],height=data[6],weight=data[7],btime=0)
-        p.add_patient()
-        data = list(filter(None,input().split(' ')))
-        while len(data) == 0:
-            print('invalid command')
-            data = list(filter(None,input().split(' ')))
+    command = data[0]
+    if command == 'add':
+        if data[1] == 'patient':
+            add_patient(data[2], data[3], data[4], data[5], data[6], data[7])
+        elif data[1] == 'visit':
+            add_visit(data[2], data[3])
 
-        
+    elif command == 'display':
+        if data[1] == 'patient':
+            display_patient(data[2])
+        elif data[1] == 'visit':
+            display_visits()
 
+    elif command == 'delete':
+        delete_patient(data[2])
 
-
-    elif data[0]== 'display' and data[1]== 'patient':
-        try:
-            id_dict[data[2]].display_patient()
-        except KeyError:
-            print('error: invalid ID')
-        data = list(filter(None,input().split(' ')))
-        while len(data) == 0:
-            print('invalid command')
-            data = list(filter(None,input().split(' ')))
-
-    
-    elif data[0]== 'add' and data[1]== 'visit':
-        try:
-            id_dict[data[2]].btime = data[3]
-            id_dict[data[2]].add_visit()
-        except KeyError:
-            print('error: invalid id')
-        data = list(filter(None,input().split(' ')))
-        while len(data) == 0:
-            print('invalid command')
-            data = list(filter(None,input().split(' ')))
-
-    
-    elif data[0]== 'delete':
-        try:
-            id_dict[data[2]].delete_patient()
-        except KeyError:
-            print('error: invalid id')
-        data = list(filter(None,input().split(' ')))
-        while len(data) == 0:
-            print('invalid command')
-            data = list(filter(None,input().split(' ')))
-
-    
-    elif data[0]=='display' and data[1]=='visit':
-        print('SCHEDULE:')
-        for x in list(visit_dict.keys()):
-            print(x)
-        data = list(filter(None,input().split(' ')))
-        while len(data) == 0:
-            print('invalid command')
-            data = list(filter(None,input().split(' ')))
-
-    
     else:
-        print('invalid command')
-        data = list(filter(None,input().split(' ')))
-        while len(data) == 0:
-            print('invalid command')
-            data = list(filter(None,input().split(' ')))
+        print('Invalid command')
 
-
+    data = list(filter(None, input().split(' ')))
+    while not is_valid_command(data):
+        print('Invalid command')
+        data = list(filter(None, input().split(' ')))
