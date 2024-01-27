a09d749d5149847175431182be524c4db8072d4c
MaHSaBaR <157877940+MaHSaBaR@users.noreply.github.com>
2024-01-27 13:37:35 +0330

    Update T4_2.py

--- ex4/T4_2.py
+++ ex4/T4_2.py
@@ -1,10 +1,10 @@
 class RoseDictionary:
     def __init__(self, initial_data=None):
         self._data = initial_data or {}
-    
+
     def __getitem__(self, key):
         return self._data[key]
-    
+
     def __setitem__(self, key, value):
         self._data[key] = value
 
@@ -13,67 +13,55 @@ class RoseDictionary:
 
     def keys(self):
         return list(self._data.keys())
-    
+
     def values(self):
         return list(self._data.values())
-    
+
     def items(self):
         return list(self._data.items())
-    
+
     def pop_item(self, raise_error=None, error_msg=None, default=None):
-        x = len(self.keys()) - 1
-        if x == -1:
-            if raise_error == True:
-                if error_msg != None:
-                    print('KeyError: \'{}\''.format(error_msg))
-                elif error_msg == None:
-                    print('KeyError: \'error_msg\'')
-            else:
-                if default != None:
-                    return default
-                else:
-                    if error_msg != None:
-                        print(error_msg)
-                    else:
-                       print('Dictionary was empty and no default value/message was specified.')
+        if not self._data:
+            self._handle_error(raise_error, error_msg, default)
         else:
-            b = self.keys()[x]
-            a = self.values()[x]
-            self.__delitem__(b)
-            return a
-    
-    def get_item(self, key, raise_error= None, error_msg= None, default= None):
+            key = self.keys()[-1]
+            value = self.values()[-1]
+            self.__delitem__(key)
+            return value
+
+    def get_item(self, key, raise_error=None, error_msg=None, default=None):
         try:
             return self._data[key]
         except KeyError:
-            if raise_error == True:
-                if error_msg != None:
-                    print('KeyError: \'{}\''.format(error_msg))
-                elif error_msg == None:
-                    print('KeyError: \'error_msg\'')
-            else:
-                if default != None:
-                    return default
-                else:
-                    if error_msg != None:
-                        print(error_msg)
-                    else:
-                       print('Value was not found and no default value/message was specified.')
+            self._handle_error(raise_error, error_msg, default)
 
-    
-        
-    
+    def _handle_error(self, raise_error, error_msg, default):
+        if raise_error:
+            if error_msg:
+                raise KeyError(f"KeyError: '{error_msg}'")
+            else:
+                raise KeyError("KeyError: 'error_msg'")
+        else:
+            if default is not None:
+                return default
+            else:
+                if error_msg:
+                    print(error_msg)
+                else:
+                    print("Error: No default value/message specified.")
+
+
+# Example usage:
 d = RoseDictionary()
 d["key1"] = "value1"
 d["key2"] = "value2"
+
 print(d["key1"])
 print(d.get_item("key1"))
-print(d.get_item("key3", default = "Default Value"))
+print(d.get_item("key3", default="Default Value"))
 d.get_item("key3")
 print(d.pop_item())
-print(d.get_item("key1", error_msg = "error message"))
-print(d.get_item("key2", error_msg = "error message2"))
+print(d.get_item("key1", error_msg="error message"))
+print(d.get_item("key2", error_msg="error message2"))
 d.pop_item()
-d.get_item("key3", default = "Default Value", raise_error = True, error_msg = "Hi")
-
-
+d.get_item("key3", default="Default Value", raise_error=True, error_msg="Hi")
