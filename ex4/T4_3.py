34c5ca1725e9aaa5ecdd642fff40fcb2bd23c7cc
MaHSaBaR <157877940+MaHSaBaR@users.noreply.github.com>
2024-01-27 13:38:28 +0330

    Update T4_3.py

--- ex4/T4_3.py
+++ ex4/T4_3.py
@@ -1,73 +1,50 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
 try:
+    # Input player names
     names = input('').split(' ')
-    player1 = names[0]
-    player2 = names[1]
-    try:
-        health = input().split(' ')
-        health1 = int(health[0])
-        health2 = int(health[1])
-        try:
-            damage = input().split(' ')
-            A = int(damage[0])
-            B = int(damage[1])
-            C = int(damage[2])
-            try:
-                round1 = input('').split(' ')
-                round2 = input('').split(' ')
-                round3 = input('').split(' ')
+    player1, player2 = names[0], names[1]
 
-                dict = {'A': A, 'B': B, 'C':C}
+    # Input player health
+    health = input().split(' ')
+    health1, health2 = int(health[0]), int(health[1])
 
-                Health1 = health1 - dict[round1[1]] - dict[round2[1]] - dict[round3[1]]
-                Health2 = health2 - dict[round1[0]] - dict[round2[0]] - dict[round3[0]]
+    # Input damage values
+    damage = input().split(' ')
+    A, B, C = int(damage[0]), int(damage[1]), int(damage[2])
 
+    # Input rounds
+    round1, round2, round3 = input('').split(' '), input('').split(' '), input('').split(' ')
 
-                s1 = 0
-                s2 = 0
-                if dict[round1[1]] > dict[round1[0]]:
-                    s2 += 1
-                elif dict[round1[1]] < dict[round1[0]]:
-                    s1 += 1
-                if dict[round2[1]] > dict[round2[0]]:
-                    s2 += 1
-                elif dict[round2[1]] < dict[round2[0]]:
-                    s1 += 1
-                if dict[round3[1]] > dict[round3[0]]:
-                    s2 += 1
-                elif dict[round3[1]] < dict[round3[0]]:
-                    s1 += 1
+    damage_dict = {'A': A, 'B': B, 'C': C}
 
+    # Calculate remaining health
+    Health1 = health1 - damage_dict[round1[1]] - damage_dict[round2[1]] - damage_dict[round3[1]]
+    Health2 = health2 - damage_dict[round1[0]] - damage_dict[round2[0]] - damage_dict[round3[0]]
 
-                print(f'{player1} -> Score: {s1}, Health: {Health1}')
-                print(f'{player2} -> Score: {s2}, Health: {Health2}')
+    # Calculate scores
+    s1 = sum(damage_dict[round1[1]] > damage_dict[round1[0]],
+             damage_dict[round2[1]] > damage_dict[round2[0]],
+             damage_dict[round3[1]] > damage_dict[round3[0]])
 
+    s2 = 3 - s1
 
-            except:
-                print('Invalid Command.')
-        except:
-            print('Invalid Command.')
-    except:
-        print('Invalid Command.')
-except:
-    print('Invalid Command.')
+    # Print results
+    print(f'{player1} -> Score: {s1}, Health: {Health1}')
+    print(f'{player2} -> Score: {s2}, Health: {Health2}')
+
+except Exception as e:
+    print(f'Error: {e}')
 
 finally:
-    with open('result.txt','w') as f:
-        f.write('{} -> Score: {}, Health: {}'.format(player1,s1,Health1))
-        f.write('\n{} -> Score: {}, Health: {}'.format(player2,s2,Health2))
-       
-    f.close()
+    # Write results to a file
+    with open('result.txt', 'w') as f:
+        f.write('{} -> Score: {}, Health: {}'.format(player1, s1, Health1))
+        f.write('\n{} -> Score: {}, Health: {}'.format(player2, s2, Health2))
 
-
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    data = {'players' : [player1,player2], 'scores':[s1,s2]}
+    # Create a bar plot
+    data = {'players': [player1, player2], 'scores': [s1, s2]}
     df = pd.DataFrame(data)
-    df.plot(kind= 'bar', x= 'players', y= 'scores')
+    df.plot(kind='bar', x='players', y='scores')
     plt.show()
-
-        
-
-
-
-
